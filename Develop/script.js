8e3ed96cb80e8471397c9f5724e8aaa4cae4cbc6
Andrew Segre <asegre18@cmc.edu>
2020-08-01 16:18:20 -0700

    final product

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-let $generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 // target each html element to make it easier later on
 const $passwordParam = document.querySelector("passwordParam");
 const $finalPassword = document.querySelector("password");
@@ -17,61 +17,75 @@ let numberBool = false;
 let lowerCaseBool = false;
 let upperCaseBool = false;
 
-passwordlength = parseInt(prompt('What length do you want your new password to be between 8 characters and 128 characters?'));
+console.log(generateBtn);
 
-while (passwordLength < 8 && passwordLength > 128) {
-  passwordlength = parseInt(prompt('Your password must be between 8 characters and 128 characters?'));
-}
+generateBtn.addEventListener("click", addingArray);
 
-while (!symbolBool && !numberBool && !lowerCaseBool && !upperCaseBool) {
-  symbolBool = confirm('Do you want your password to have Special Characters?');
-  numberBool = confirm('Do you want your password to have lowercase letters?');
-  lowerCaseBool = confirm('Do you want your password to have uppercase letters?');
-  upperCaseBool = confirm('Do you want your password to have numbers');
-}
+function addingArray() {
+  passwordLength = parseInt(prompt('What length do you want your new password to be between 8 characters and 128 characters?'));
 
-// If statements for each case
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt('Your password must be between 8 characters and 128 characters, Please enter a new password Length.'));
+    console.log(passwordLength);
+  }
+  while (!symbolBool && !numberBool && !lowerCaseBool && !upperCaseBool) {
+    symbolBool = confirm('Do you want your password to have Special Characters?');
+    numberBool = confirm('Do you want your password to have lowercase letters?');
+    lowerCaseBool = confirm('Do you want your password to have uppercase letters?');
+    upperCaseBool = confirm('Do you want your password to have numbers');
+  }
+  // If statements for each case
+  if (symbolBool) {
+    // availableChar = add symbols to the Array...
+    for (let i = 0; i < symbols.length; i++) {
+      availableChar.push(symbols[i]);
+    }
+    console.log(availableChar);
+  }
+  if (numberBool) {
+    // availableChar = add symbols to the Array...
+    for (let i = 0; i < numbers.length; i++) {
+      availableChar.push(numbers[i]);
+    }
+    console.log(availableChar);
+  }
+  if (lowerCaseBool) {
+    // availableChar = add symbols to the Array...
+    for (let i = 0; i < lowerCase.length; i++) {
+      availableChar.push(lowerCase[i]);
+    }
+    console.log(availableChar);
+  }
+  if (upperCaseBool) {
+    // availableChar = add symbols to the Array...
+    for (let i = 0; i < upperCase.length; i++) {
+      availableChar.push(upperCase[i]);
+    }
+    console.log(availableChar);
+  }
 
-if (symbolBool) {
-  // availableChar = add symbols to the Array...
-  availableChar = symbols.concat();
-  console.log(availableChar);
+  for (let i = 0; i < passwordLength; i++) {
+    password = availableChar[i];
+    // make it randomly select an index from the available charcters
+  }
+  writePassword();
 }
-if (numberBool) {
-  // availableChar = add symbols to the Array...
-  availableChar.push(numbers);
-  console.log(availableChar);
-}
-if (lowerCaseBool) {
-  // availableChar = add symbols to the Array...
-  availableChar.push(lowerCase);
-  console.log(availableChar);
-}
-if (upperCaseBool) {
-  // availableChar = add symbols to the Array...
-  availableChar.push(upperCase);
-  console.log(availableChar);
-}
-
-for (let i = 0; i < passwordLength; i++) {
-  password = availableChar[i];
-  // make it randomly select an index from the available charcters
-}
-
 // Write password to the #password input
 function writePassword() {
   password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-$generateBtn.addEventListener("click", function () {
-  password = generatePassword()
-});
 function generatePassword() {
-  return password;
+  let newPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let randomChar = Math.floor(Math.random() * availableChar.length);
+    newPassword += availableChar[randomChar];
+  }
+  return newPassword;
 }
 
+// possibly add an initial prompt
 // alert the prompt instructions once the prompt button is clicked
 // promptParameter = $passwordParam.addEventListener("click", function () {
 //   alert('Criteria\nThis tool will generate a password for you with the criteria you select\nYou will be selecting which criteria to include in the password\nYou will be selecting the length of the password\nYou may type lowercase, uppercase, numeric values\nYou may select special characters\nYou will be selecting the length of the password');
